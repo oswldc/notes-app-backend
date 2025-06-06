4d3f22bd7d5ca7adf1537eda500a9f6278381bb2
oswld <oswld.dc@gmail.com>
2025-06-07 00:45:51 +0800

    implementasi postgres database

--- src/server.js
+++ src/server.js
@@ -1,45 +1,47 @@
-const Hapi = require("@hapi/hapi");
-const notes = require("./api/notes");
-const NotesService = require("./services/inMemory/NotesService");
-const NotesValidator = require("./validator/notes");
-const ClientError = require("./exceptions/ClientError");
+require('dotenv').config();
+
+const Hapi = require('@hapi/hapi');
+const notes = require('./api/notes');
+const NotesService = require('./services/postgres/NotesService');
+const NotesValidator = require('./validator/notes');
+const ClientError = require('./exceptions/ClientError');
 
 const init = async () => {
-    const notesService = new NotesService();
-    const server = Hapi.server({
-        port: 3000,
-        host: process.env.NODE_ENV !== "production" ? "localhost" : "0.0.0.0",
-        routes: {
-            cors: {
-                origin: ["*"],
-            },
-        },
-    });
+  const notesService = new NotesService();
+  const server = Hapi.server({
+    port: process.env.PORT,
+    host: process.env.HOST,
+    routes: {
+      cors: {
+        origin: ['*'],
+      },
+    },
+  });
 
-    await server.register({
-        plugin: notes,
-        options: {
-            service: notesService,
-            validator: NotesValidator,
-        },
-    });
+  await server.register({
+    plugin: notes,
+    options: {
+      service: notesService,
+      validator: NotesValidator,
+    },
+  });
 
-    server.ext("onPreResponse", (request, h) => {
-        const { response } = request;
+  server.ext('onPreResponse', (request, h) => {
+    const { response } = request;
 
-        if (response instanceof ClientError) {
-            const newResponse = h.response({
-                status: "fail",
-                message: response.message,
-            });
-            newResponse.code(response.statusCode);
-            return newResponse;
-        }
-        return h.continue;
-    });
+    if (response instanceof ClientError) {
+      const newResponse = h.response({
+        status: 'fail',
+        message: response.message,
+      });
+      newResponse.code(response.statusCode);
+      return newResponse;
+    }
+    return h.continue;
+  });
 
-    await server.start();
-    console.log(`Server berjalan pada ${server.info.uri}`);
+  await server.start();
+  console.log(`Server berjalan pada ${server.info.uri}`);
 };
 
 init();
