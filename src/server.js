fa26bbc0f0531fbd19502eb29e893960ffe58b7d
oswld <oswld.dc@gmail.com>
2025-06-06 16:55:19 +0800

    Menambahkan OnPreRespon

--- src/server.js
+++ src/server.js
@@ -1,30 +1,46 @@
-const Hapi = require('@hapi/hapi');
-const notes = require('./api/notes');
-const NotesService = require('./services/inMemory/NotesService');
-const NotesValidator = require('./validator/notes');
+const Hapi = require("@hapi/hapi");
+const notes = require("./api/notes");
+const NotesService = require("./services/inMemory/NotesService");
+const NotesValidator = require("./validator/notes");
+const ClientError = require("./exceptions/ClientError");
 
 const init = async () => {
-  const notesService = new NotesService();
-  const server = Hapi.server({
-    port: 3000,
-    host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
-    routes: {
-      cors: {
-        origin: ['*'],
-      },
-    },
-  });
+    const notesService = new NotesService();
+    const server = Hapi.server({
+        port: 3000,
+        host: process.env.NODE_ENV !== "production" ? "localhost" : "0.0.0.0",
+        routes: {
+            cors: {
+                origin: ["*"],
+            },
+        },
+    });
 
-  await server.register({
-    plugin: notes,
-    options: {
-      service: notesService,
-      validator: NotesValidator,
-    },
-  });
+    await server.register({
+        plugin: notes,
+        options: {
+            service: notesService,
+            validator: NotesValidator,
+        },
+    });
 
-  await server.start();
-  console.log(`Server berjalan pada ${server.info.uri}`);
+    server.ext("onPreResponse", (request, h) => {
+        const { response } = request;
+
+        if (response instanceof ClientError) {
+            const newResponse = h.response({
+                status: "fail",
+                message: response.message,
+            });
+            newResponse.code(response.statusCode);
+            return newResponse;
+        }
+        return h.continue;
+    });
+
+    await server.start();
+    console.log(`Server berjalan pada ${server.info.uri}`);
 };
 
 init();
+
